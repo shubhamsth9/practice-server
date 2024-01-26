512862a6671ab4d274ef04c700c575bbd5be8d98
shubhamxthakur <stfushubham@gmail.com>
2024-01-26 14:28:49 +0530

    express installed, server created

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        msg : "This is a message in response to a successful GET request"
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+})
