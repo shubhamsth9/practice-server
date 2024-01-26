46ff3d137fda622b9336406d5bfecb78e47fa5b1
shubhamxthakur <stfushubham@gmail.com>
2024-01-26 14:53:24 +0530

    added start script in package.json

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.status(200).json({
         msg : "This is a message in response to a successful GET request"
